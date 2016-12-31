eaae6311f4693635126f7c92fc8314ff04114afb
Mohammad1993i <m.bagher137188@gmail.com>
2017-01-01 01:02:44 +0330

    Update Luncher.php

--- Luncher.php
+++ Luncher.php
@@ -1,5 +1,5 @@
 <?php
-define('BOT_TOKEN', '@ Mohammad1993i!');
+define('BOT_TOKEN', '**@ Mohammad1993i!**');
 define('API_URL', 'https://api.telegram.org/bot'.BOT_TOKEN.'/');
 
 function apiRequestWebhook($method, $parameters) {
